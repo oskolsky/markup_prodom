eca4c6fdd56aea91335fb1c49e634c30e673164d
Maxim Oskol'sky <info@oskolsky.com>
2014-12-15 01:42:37 +0300

    Install Chemistry Template

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-markup_prodom
-=============
+# Chemistry Template
+Version: 1.0 beta, 12.12.2014
+
+## Introduction
+Chemistry Template is not a framework. It's a development kit that provides the most basic frontend functionality as a foundation, but is ready to adopt almost all your needs.
+
+## Howto
+[Documentation](docs)
+
+## Credits
+* @brainsapiens
+* @oskolsky
---
+++ gulpfile.js
@@ -0,0 +1,253 @@
+//****************************************************************************************************
+//
+// .. VARIABLES
+//
+//****************************************************************************************************
+var
+  del = require('del'),
+  gulp = require('gulp'),
+  plugin = require('gulp-load-plugins')(),
+  path = require('./path.json');
+
+
+
+//****************************************************************************************************
+//
+// .. TASKS
+//
+//****************************************************************************************************
+//
+// .. Clean
+//
+gulp.task('clean', function(cb) {
+  del(path.build, cb);
+});
+
+//
+// .. Layouts
+//
+gulp.task('layouts:d', function() {
+  gulp.src([path.layouts.all, path.layouts.views.none])
+    .pipe(plugin.rubyHaml())
+    .pipe(gulp.dest(path.build));
+});
+
+gulp.task('layouts:s', function() {
+  gulp.src([path.layouts.all, path.layouts.views.none])
+    .pipe(plugin.rubyHaml())
+    .pipe(gulp.dest(path.build));
+});
+
+gulp.task('layouts:p', function() {
+  gulp.src([path.layouts.all, path.layouts.views.none])
+    .pipe(plugin.rubyHaml())
+    .pipe(plugin.htmlReplace({javascripts: {src: path.javascripts.app, tpl: '<script src=\'%s\'></script>'}}))
+    .pipe(plugin.minifyHtml({conditionals: true}))
+    .pipe(gulp.dest(path.build));
+});
+
+//
+// .. Stylesheets
+//
+gulp.task('stylesheets:d', function() {
+  gulp.src(path.stylesheets.app)
+    .pipe(plugin.rubySass({noCache: true}))
+    .pipe(gulp.dest(path.stylesheets.dest));
+});
+
+gulp.task('stylesheets:s', function() {
+  gulp.src(path.stylesheets.app)
+    .pipe(plugin.rubySass({noCache: true}))
+    .pipe(plugin.autoprefixer('last 2 versions'))
+    .pipe(gulp.dest(path.stylesheets.dest));
+});
+
+
+
+
+
+gulp.task('stylesheets:p', function() {
+  gulp.src(path.stylesheets.app)
+    .pipe(plugin.rubySass({noCache: true}))
+    .pipe(plugin.autoprefixer('last 2 versions'))
+    .pipe(plugin.concatCss('application.css'))
+    .pipe(plugin.minifyCss({processImport: false}))
+    .pipe(gulp.dest(path.stylesheets.dest));
+});
+
+
+
+
+
+gulp.task('stylesheets:v', function() {
+  gulp.src(path.stylesheets.vendor.all)
+    .pipe(gulp.dest(path.stylesheets.vendor.dest));
+});
+
+//
+// .. Javascripts
+//
+gulp.task('javascripts:d', function() {
+  gulp.src(path.javascripts.all)
+    .pipe(gulp.dest(path.javascripts.dest));
+});
+
+gulp.task('javascripts:s', function() {
+  gulp.src(path.javascripts.all)
+    .pipe(gulp.dest(path.javascripts.dest));
+  gulp.src([path.javascripts.all, path.javascripts.vendor.none])
+    .pipe(plugin.jshint())
+    .pipe(plugin.jshint.reporter());
+});
+
+gulp.task('javascripts:p', function() {
+  gulp.src([path.javascripts.all, path.javascripts.vendor.none])
+    .pipe(plugin.jshint())
+    .pipe(plugin.jshint.reporter());
+  gulp.src(path.javascripts.list)
+    .pipe(plugin.concat('application.js'))
+    .pipe(plugin.uglify())
+    .pipe(gulp.dest(path.javascripts.dest));
+});
+
+//
+// .. Images
+//
+gulp.task('images:d', function() {
+  gulp.src(path.images.all)
+    .pipe(gulp.dest(path.images.dest));
+});
+
+gulp.task('images:s', function() {
+  gulp.src(path.images.all)
+    .pipe(plugin.imagemin())
+    .pipe(gulp.dest(path.images.dest));
+});
+
+gulp.task('images:p', function() {
+  gulp.src(path.images.all)
+    .pipe(plugin.imagemin())
+    .pipe(gulp.dest(path.images.dest));
+});
+
+//
+// .. Copy
+//
+gulp.task('copy:fonts', function() {
+  gulp.src(path.fonts.all)
+    .pipe(gulp.dest(path.fonts.dest));
+});
+
+gulp.task('copy:files', function() {
+  gulp.src(path.files.list)
+    .pipe(gulp.dest(path.build));
+});
+
+//
+// .. Connect
+//
+gulp.task('connect', function() {
+  plugin.connect.server({
+    root: [path.build],
+    port: 1111
+  });
+});
+
+//
+// .. Watch
+//
+gulp.task('watch', function() {
+  // .. Layouts
+  gulp.src([path.layouts.all, path.layouts.views.none])
+    .pipe(plugin.watch([path.layouts.all, path.layouts.views.none]))
+    .pipe(plugin.rubyHaml())
+    .pipe(gulp.dest(path.build));
+
+  gulp.watch(path.layouts.views.all, ['layouts:d']);
+
+  // .. Stylesheets
+  gulp.watch(path.stylesheets.all, ['stylesheets:d']);
+
+  gulp.src(path.stylesheets.vendor.all)
+    .pipe(plugin.watch(path.stylesheets.vendor.all))
+    .pipe(gulp.dest(path.stylesheets.vendor.dest));
+  
+  // .. Javascripts
+  gulp.src(path.javascripts.all)
+    .pipe(plugin.watch(path.javascripts.all))
+    .pipe(gulp.dest(path.javascripts.dest));
+
+  // .. Images
+  gulp.src(path.images.all)
+    .pipe(plugin.watch(path.images.all))
+    .pipe(gulp.dest(path.images.dest));
+
+  // .. Fonts
+  gulp.src(path.fonts.all)
+    .pipe(plugin.watch(path.fonts.all))
+    .pipe(gulp.dest(path.fonts.dest));
+
+  // .. Files
+  gulp.src(path.files.list)
+    .pipe(plugin.watch(path.files.list))
+    .pipe(gulp.dest(path.build));
+});
+
+
+
+//****************************************************************************************************
+//
+// .. RUN
+//
+//****************************************************************************************************
+gulp.task('default', function() {
+  gulp.start(
+    'connect',
+    'watch'
+  );
+});
+
+//
+// .. Development
+//
+gulp.task('d', ['clean'], function() {
+  gulp.start(
+    'layouts:d',
+    'stylesheets:d',
+    'stylesheets:v',
+    'javascripts:d',
+    'images:d',
+    'copy:fonts',
+    'copy:files'
+  );
+});
+
+//
+// .. Staging
+//
+gulp.task('s', ['clean'], function() {
+  gulp.start(
+    'layouts:s',
+    'stylesheets:s',
+    'stylesheets:v',
+    'javascripts:s',
+    'images:s',
+    'copy:fonts',
+    'copy:files'
+  );
+});
+
+//
+// .. Production
+//
+gulp.task('p', ['clean'], function() {
+  gulp.start(
+    'layouts:p',
+    'stylesheets:p',
+    'stylesheets:v',
+    'javascripts:p',
+    'images:p',
+    'copy:fonts',
+    'copy:files'
+  );
+});
